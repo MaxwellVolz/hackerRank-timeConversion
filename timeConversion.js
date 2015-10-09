907d9f78635202662e35ef9f53b1b700e4cde7ee
Maxwell Volz <maxwellvolz@gmail.com>
2015-10-08 22:24:47 -0700

    initial commit

---
+++ timeConversion.js
@@ -0,0 +1,29 @@
+// time conversion exercise
+//
+// max
+
+var converter = function(t){
+
+	var out = ("Time is: " + t);
+	var time = t.slice(0,8),
+		suffix = t.slice(8,10),
+		hours = t.slice(0,2),
+		noHours = t.slice(2,8);
+	
+	if(suffix == "AM"){
+		return time;
+	}	
+	else{
+		return (parseInt(hours) + 12) + noHours;
+			
+	}
+	
+	//return suffix + time + "\n" + hours;
+}
+
+
+
+
+console.log(converter("12:00:00AM"));
+console.log(converter("01:00:00PM"));
+
